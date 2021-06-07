3a38c9dc53726726667c41429324da000a25c4b0
Valentín Castravete Georgian <58530982+valentincastravete@users.noreply.github.com>
2021-06-07 09:44:59 +0200

    [FIX] Alphabetic order on init import

--- car_workshop_base/models/__init__.py
+++ car_workshop_base/models/__init__.py
@@ -2,6 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
 from . import fleet_vehicle
-from . import repair_line
 from . import fleet_vehicle_model
+from . import repair_line
 from . import repair_order
