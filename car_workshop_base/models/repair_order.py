b7df40c3bcab0cde0b10858d8ea4fd56eba9d34c
amartinez <albertisho99@gmail.com>
2022-02-21 10:28:19 +0100

    [FIX] or_grouping: override the python _create_invoices method
    [FIX] or_grouping: override the python _create_invoices method
    
    [FIX] flake8

--- car_workshop_base/models/repair_order.py
+++ car_workshop_base/models/repair_order.py
@@ -168,3 +168,45 @@ class RepairOrder(models.Model):
             self.partner_id = self.vehicle_id.driver_id
             self.product_id = self.vehicle_id.product_id
             self.repair_type = 'vehicle_repair'
+
+    def _create_invoices(self, group=False):
+        moves = super(RepairOrder, self)._create_invoices(group)
+
+        if group:
+            repairs = list(
+                self.env['repair.order'].search(
+                    [['id', 'in', list(moves.keys())]]
+                    )
+                )
+            invoice = self.env['account.move'].search(
+                [['id', 'in', list(moves.values())]]
+                )
+            line_names = [line.name for line in invoice.invoice_line_ids]
+
+            i = 0
+            new_lines = 0
+            while i < len(invoice.invoice_line_ids) - new_lines:
+                print(len(invoice.invoice_line_ids) - new_lines)
+                print(line_names[i])
+                for repair in repairs:
+                    if repair.name in line_names[i]:
+                        invoice.write({
+                            'invoice_line_ids':  [(0, 0, {
+                                'name': repair.name,
+                                'display_type': 'line_section',
+                                'sequence': i + new_lines + 1,
+                            })]})
+                        new_lines = new_lines + 1
+                        if repair.quotation_notes:
+                            invoice.write({
+                                'invoice_line_ids':  [(0, 0, {
+                                    'name': repair.quotation_notes,
+                                    'display_type': 'line_note',
+                                    'sequence': i + new_lines + 1,
+                                })]})
+                            new_lines = new_lines + 1
+                        repairs.remove(repair)
+                invoice.invoice_line_ids[i].write({'sequence': i+new_lines+1})
+                i = i + 1
+            invoice.write({'narration': ''})
+        return moves
